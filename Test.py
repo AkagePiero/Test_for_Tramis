8133e5b02c6b03e3440ac1e8154c2acd91ae4ab4
AkagePiero <leon131999@mail.ru>
2023-08-15 12:15:04 +0300

    import sqlalhemy

--- Test.py
+++ Test.py
@@ -1,8 +1,9 @@
 import psycopg2
-
+import sqlalchemy
 conn = psycopg2.connect(dbname='postgres', user='postgres', 
-                        password='12345', host='localhost')
+                        password='12345', host='localhost', port="5432")
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM Test LIMIT 10')
-records = cursor.fetchall()
-print(records)
+cursor.execute('SELECT * FROM public."Test"')
+rows = cursor.fetchall()
+
+print(rows)
