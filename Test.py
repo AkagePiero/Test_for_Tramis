02fb0b8af520148b3f16a2f4660f98f4978d0172
AkagePiero <leon131999@mail.ru>
2023-08-15 12:04:07 +0300

    imports psycopg2

--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
-print('hi')
+import psycopg2
+
+conn = psycopg2.connect(dbname='postgres', user='postgres', 
+                        password='12345', host='localhost')
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM Test LIMIT 10')
+records = cursor.fetchall()
+print(records)
